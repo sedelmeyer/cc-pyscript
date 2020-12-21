6f10fd85e61d83751ccace6e2957eb6376f41ed1
sedelmeyer <20605812+sedelmeyer@users.noreply.github.com>
2020-12-21 09:41:07 -0500

    hooks(pre): change package_name to script_name

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,6 +1,6 @@
 """
 This script contains the cookiecutter hooks that run immediately before the
-generation of the ``cc-pydata`` template, but after completing the intial
+generation of the template, but after completing the intial
 build prompts.
 
 For more information about cookiecutter hooks, please see:
@@ -14,14 +14,14 @@ import sys
 
 MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
 
-module_name = '{{ cookiecutter.package_name}}'
+module_name = '{{ cookiecutter.script_name}}'
 
 if not re.match(MODULE_REGEX, module_name):
-    """Ensure the template has been given a valid package_name"""
+    """Ensure the template has been given a valid script_name"""
     print(
         'ERROR: The package name ({}) is not a valid Python module name. '
         'Please do not use a - and use _ instead'.format(module_name)
     )
 
-    #Exit to cancel project
+    # Exit to cancel project
     sys.exit(1)
